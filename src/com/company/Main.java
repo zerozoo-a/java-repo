de281432e65f68ab41dd8d232e5975fe4d54fad5
윤영주 <zoo@yun-yeongjus-MacBook-Air.local>
2022-02-14 01:09:44 +0900

    faet: read file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         getTyping();
+        readFile("out.txt");
     }
 
     public static void getTyping(){
@@ -18,5 +21,18 @@ public class Main {
         for(int e:store){
             System.out.println("stored value is : "+e);
         }
+    }
+    public static void readFile(String fileName){
+        try{
+            File file = new File(fileName);
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextInt()){
+                System.out.println(sc.nextInt());
+            }
+            sc.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 }
