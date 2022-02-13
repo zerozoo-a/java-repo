18bdcccc35b88e8f691e4b78c751a53bab81a234
윤영주 <zoo@yun-yeongjus-MacBook-Air.local>
2022-02-14 01:02:16 +0900

    feat: add method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        getTyping();
+    }
+
+    public static void getTyping(){
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        System.out.println(i*5);
+        int[] store = new int[5];
+        int cnt = 0;
+        while(sc.hasNextInt() && cnt < 5){
+            store[cnt]=sc.nextInt();
+            cnt++;
+        }
+        sc.close();
+        for(int e:store){
+            System.out.println("stored value is : "+e);
+        }
     }
 }
-
